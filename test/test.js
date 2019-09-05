6441a128de5528dd827a1844d8d9f87a088793b5
Divy Srivastava <dj.srivastava23@gmail,com>
2019-09-05 09:23:53 +0530

    Add tests for post() function

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var Torpedo = require('../');
-var { get } = new Torpedo({ name: "test" });
+var instance = new Torpedo({ name: "test" });
+var { get, post } = instance;
 var path = require("path");
 
 describe('Torpedo', () => {
@@ -8,11 +9,15 @@ describe('Torpedo', () => {
     })
     it("Create new instance", done => {
         if (get) return done();
+    });
+    it("Change configuration", done => {
+        instance.name("Hi");
+        done(instance.options.name == "Hi");
     })
   
 })
 
-describe('HTTP Get', () => {
+describe('HTTP: Get', () => {
     it("send()", done => {
         get('/').send("Hello");
         done();
@@ -45,8 +50,43 @@ describe('HTTP Get', () => {
     it("sendFile()", done => {
       get('/sendFile').sendFile(path.join(__dirname, './test.js'));
       done();
+    })        
+});
+
+describe('HTTP: Post', () => {
+    it("send()", done => {
+        post('/').send("Hello");
+        done();
+    });
+    it("wait()", done => {
+        post('/wait').wait((req, res) => {
+            res.send('/wait')
+        }, 1000);
+        done();
+    })
+    it("redirect()", done => {
+        post('/redirect').redirect("/");
+        done();
+    })
+    it("status()", done => {
+        post('/status').status(404);
+        done();
+    })
+    it("do()", done => {
+        post('/do_simple').do("Hello");
+        post('/do').do((req, res, next) => {
+            res.send("Hello")
+        })
+        done();
+    })
+    it("pass()", done => {
+        post('/pass').pass();
+        done();
+    })
+    it("sendFile()", done => {
+      post('/sendFile').sendFile(path.join(__dirname, './test.js'));
+      done();
     })
     
         
 });
-
