7f0753c3cdfe6fed93a44d19545e80cd4a7e2841
Divy Srivastava <dj.srivastava23@gmail,com>
2019-09-05 09:22:11 +0530

    Add config change functions

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var Spring = require('js-spring');
 var log = console.log;
 class init {
 	constructor(opt) {
-		this.app = new Spring(opt).app;
+		var instance = new Spring(opt);
+		this.app = instance.app;
+		this.options = instance.options;
+		for (var x in opt) {
+		   this[x] = (set) => {
+				instance.options[x] = set;
+				return true;
+			}	
+		}		
 		this.get = require('./src/get')(this.app).get;
+		this.post = require('./src/post')(this.app).post;
 	   module.exports = this;
 	}
 }
