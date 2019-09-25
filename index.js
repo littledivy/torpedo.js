0f1c3cb5200794bb6ce5ee33dd6e1f2816ece0c5
Divy Srivastava <dj.srivastava23@gmail.com>
2019-09-25 18:16:09 +0530

    Spring js wrapper

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-var Spring = require('js-spring');
 var log = console.log;
 class init {
-	constructor(opt) {
-		var instance = new Spring(opt);
-		this.app = instance.app;
-		this.options = instance.options;
+	constructor(instance,opt) {
+		this.app = instance;
+		/** Do Not Delete
+                this.options = instance.options;
 		for (var x in opt) {
 		   this[x] = (set) => {
 				instance.options[x] = set;
 				return true;
 			}	
-		}		
+	        } **/	
 		this.get = require('./src/get')(this.app).get;
 		this.post = require('./src/post')(this.app).post;
-		this.socket = instance.socket;
 		if(opt.mongo) this.mongo = require('./src/mongo')(opt.mongo);
 		if(opt.client) this.socket = require('socket.io-client')
 	   module.exports = this;
